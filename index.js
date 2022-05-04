5698066671c732d656bedf8da5cf2c915f55294f
danielkri <dlkn87@gmail.com>
2022-05-04 19:57:08 +0300

    created player and enemy
    created player and enemy

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.width = 50;
+    this.height = 150;
   }
 
   draw() {
     c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, 150);
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
   }
 }
 
@@ -25,11 +41,9 @@ const player = new Sprite({
   },
   velocity: {
     x: 0,
-    y: 0,
+    y: 10,
   },
 });
-
-player.draw();
 
 const enemy = new Sprite({
   position: {
@@ -38,12 +52,17 @@ const enemy = new Sprite({
   },
   velocity: {
     x: 0,
-    y: 0,
+    y: 10,
   },
 });
 
-enemy.draw();
-
 function animate() {
   window.requestAnimationFrame(animate);
+
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
 }
+
+animate();
