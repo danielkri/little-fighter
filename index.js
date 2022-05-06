792e9b74334850402a68d443ea46f7be0105c9ef
danielkri <dlkn87@gmail.com>
2022-05-06 23:38:35 +0300

    shop sprite with animation
    shop sprite with animation

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ const background = new Sprite({
   },
   imageSrc: "./assets/background.png",
 });
+
+const shop = new Sprite({
+  position: {
+    x: 600,
+    y: 128,
+  },
+  imageSrc: "./assets/shop.png",
+  scale: 2.75,
+  framesMax: 6,
+  framesHold: 5,
+});
+
 const player = new Fighter({
   position: {
     x: 0,
@@ -75,6 +87,7 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
+  shop.update();
   player.update();
   enemy.update();
 
