cbe6cdfc611313f45629be2f966f2e0313b93543
danielkri <dlkn87@gmail.com>
2022-05-06 22:52:13 +0300

    code cleaning and background sprite
    code cleaning and background sprite

---
+++ js/utils.js
@@ -0,0 +1,36 @@
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
+}
+
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId);
+  document.querySelector("#displayText").style.display = "flex";
+  let matchResult;
+  if (player.health === enemy.health) matchResult = "Tie";
+  else if (player.health > enemy.health) matchResult = "Player 1 Wins";
+  else if (player.health < enemy.health) matchResult = "Player 2 Wins";
+
+  document.querySelector("#displayText").innerHTML = matchResult;
+}
+
+let timer = 61;
+let timerId;
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+
+  if (timer === 0) {
+    determineWinner({ player, enemy });
+  }
+}
