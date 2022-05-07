f15d4d69477a0f04684fd88b078ca60944cc2eed
danielkri <dlkn87@gmail.com>
2022-05-07 23:23:25 +0300

    interface design and animation
    interface design and animation

--- index.js
+++ index.js
@@ -172,6 +172,8 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   shop.update();
+  c.fillStyle = "rgba(255,255,255,0.15)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
 
@@ -223,7 +225,10 @@ function animate() {
   ) {
     player.isAttacking = false;
     enemy.takeHit();
-    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+    // document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+    gsap.to("#enemyHealth", {
+      width: enemy.health + "%",
+    });
   }
 
   //if player misses
@@ -241,7 +246,10 @@ function animate() {
   ) {
     enemy.isAttacking = false;
     player.takeHit();
-    document.querySelector("#playerHealth").style.width = player.health + "%";
+    // document.querySelector("#playerHealth").style.width = player.health + "%";
+    gsap.to("#playerHealth", {
+      width: player.health + "%",
+    });
   }
 
   //if enemy misses
